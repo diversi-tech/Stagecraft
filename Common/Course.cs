36e1da7561e927dd7ae863c98809828a6985ddd2
Tehila <t0583202927>
2024-06-19 12:22:59 +0300

    add

--- Common/Course.cs
+++ Common/Course.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +9,38 @@ namespace Common
 {
     public class Course
     {
-        public int Code { get; set; }
-        public string Name { get; set; }
-        public string NumberOfChapters { get; set; }
+        public int courses_id { get; set; }
+        public string courses_name { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        
         public int Price { get; set; }
-        public List<Recommendation> Recommendations { get; set; }
+        //public List<Recommendation> Recommendations { get; set; }
+        public int numberOfViewers  { get; set; }
+        //public Timer Length { get; set;}
+        //public DateTime create_at { get; set; }
+        //public DateTime update_at { get; set; }
+
+
+        public Course MapCourse(IDataReader reader)
+        {
+            if (reader.Read())
+            {
+                Course course = new Course();
+
+                course.courses_id = Convert.ToInt32(reader["courses_id"]);
+                course.courses_name = reader["courses_name"].ToString();
+                course.title = reader["title"].ToString();
+                course.description = reader["description"].ToString();
+                course.Price = Convert.ToInt32(reader["Price"]);
+                //Recommendations = new List<Recommendation>();
+
+                return course;
+            }
+            return null;
+        }
 
 
 
-
-        public string Description { get; set; }
     }
 }
--- StagecraftDAL/Services/CourseService.cs
+++ StagecraftDAL/Services/CourseService.cs
@@ -1,15 +1,20 @@
 ﻿using Common;
 using System;
 using System.Collections.Generic;
-using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 
 namespace StagecraftDAL.Services
 {
     internal class CourseService
     {
+        public Course GetAllCourses()
+        {
+        return  ExecuteStoredProcedure("getAllCourses",MapCourse,null) ;
+        }
+
 
     }
 }
