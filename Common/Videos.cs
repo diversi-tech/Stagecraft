48c6076e1427466986a969b67970d3e5b62942a1
Avigail Benifla <avigail3435@gmail.com>
2024-07-01 13:09:17 +0300

    more tables for the commom

---
+++ Common/Videos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Videos
+    {
+        public int video_id { get; set; }
+        public int courses_id { get; set; }
+        public string video_name { get; set; }
+        public int video_length { get; set; }
+ }
+
+    }
+
