8baf16aac2ddc9482545172b8c1e1b6494bdfb81
AyalaGiat <giat9917938@gmail.com>
2024-07-17 12:56:39 +0300

    qq

--- Common/Users.cs
+++ Common/Users.cs
@@ -3,11 +3,11 @@
     public class Users
     {
         public int Code { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public string Email { get; set; }
         public int CoursesNum { get; set; }
         public bool IsConfirmed { get; set; }
-        public string LastView { get; set; }
+        public string? LastView { get; set; }
         public string Password { get; set; }
         public DateTime RegistrationDate { get; set; }
         public DateTime LastModifiedDate { get; set; }
