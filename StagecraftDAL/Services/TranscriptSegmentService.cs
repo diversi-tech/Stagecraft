aaa040e1722cb03aa7b149f47fbbb79518c7ab50
rivkamos <77576647+rivkamos@users.noreply.github.com>
2024-08-06 22:34:54 +0300

    change to postgreSQL

--- StagecraftDAL/Services/TranscriptSegmentService.cs
+++ StagecraftDAL/Services/TranscriptSegmentService.cs
@@ -8,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Common;
+using Npgsql;
 
 namespace StagecraftDAL.Services
 {
@@ -22,30 +23,8 @@ namespace StagecraftDAL.Services
 
         public List<TranscriptSegment> GetTranscriptionByVideoId(int videoId)
         {
-            List<TranscriptSegment> segments = new List<TranscriptSegment>();
-            using (var connection = new SqlConnection(_connectionString))
-            using (var command = new SqlCommand("GetTranscriptionByVideoId", connection))
-            {
-                command.CommandType = CommandType.StoredProcedure;
-                command.Parameters.AddWithValue("@VideoId", videoId);
-
-                connection.Open();
-                using (var reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        segments.Add(new TranscriptSegment
-                        {
-                            Id = (int)reader["Id"],
-                            VideoId = (int)reader["VideoId"],
-                            Time = (int)reader["Time"],
-                            Text = (string)reader["Text"],
-                            CreatedAt = (DateTime)reader["CreatedAt"],
-                            UpdatedAt = (DateTime)reader["UpdatedAt"]
-                        });
-                    }
-                }
-            }
+            NpgsqlParameter param1 = new NpgsqlParameter("pvideo_id", videoId);
+            var segments = PostgreSQLDataAccess.ExecuteFunction<TranscriptSegment>("get_transcription_by_video_id", param1);
             return segments;
         }
 
--- StagecraftDAL/Services/VideoService.cs
+++ StagecraftDAL/Services/VideoService.cs
@@ -14,12 +14,6 @@ namespace StagecraftDAL.Services
 {
     public class VideoService : IVideoService
     {
-        private readonly string _connectionString;
-
-        public VideoService(IConfiguration configuration)
-        {
-            _connectionString = configuration.GetConnectionString("DefaultConnection");
-        }
         public Videos GetVideoById(int videoId)
         {
             NpgsqlParameter param1 = new NpgsqlParameter("pvideo_id", videoId);
@@ -30,30 +24,8 @@ namespace StagecraftDAL.Services
 
         public List<TranscriptSegment> GetTranscriptionByVideoId(int videoId)
         {
-            List<TranscriptSegment> segments = new List<TranscriptSegment>();
-            using (var connection = new SqlConnection(_connectionString))
-            using (var command = new SqlCommand("GetTranscriptionByVideoId", connection))
-            {
-                command.CommandType = CommandType.StoredProcedure;
-                command.Parameters.AddWithValue("@VideoId", videoId);
-
-                connection.Open();
-                using (var reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        segments.Add(new TranscriptSegment
-                        {
-                            Id = (int)reader["Id"],
-                            VideoId = (int)reader["VideoId"],
-                            Time = (int)reader["Time"],
-                            Text = (string)reader["Text"],
-                            CreatedAt = (DateTime)reader["CreatedAt"],
-                            UpdatedAt = (DateTime)reader["UpdatedAt"]
-                        });
-                    }
-                }
-            }
+            NpgsqlParameter param1 = new NpgsqlParameter("pvideo_id", videoId);
+            var segments = PostgreSQLDataAccess.ExecuteFunction<TranscriptSegment>("get_transcription_by_video_id", param1);
             return segments;
         }
     }
